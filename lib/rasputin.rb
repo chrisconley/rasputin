26b0befa31aacaac75300798040dcc746e645c6c
tchak <paul@chavard.net>
2011-09-09 20:24:35 +0200

    remove unused code

--- lib/rasputin.rb
+++ lib/rasputin.rb
@@ -7,9 +7,6 @@ require "rasputin/spade_dependencies_processor"
 
 module Rasputin
   class Engine < ::Rails::Engine
-    config.closure = ActiveSupport::OrderedOptions.new
-    config.closure.lib = 'vendor/assets/closure-library/closure'
-
     initializer :setup_rasputin do |app|
       app.assets.register_preprocessor 'application/javascript', Rasputin::SpadeDependenciesProcessor
       app.assets.register_engine '.handlebars', Rasputin::HandlebarsTemplate
